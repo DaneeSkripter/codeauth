1d4906f9a6029ad609e259274f3dc32d6bc23936
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-05-01 12:56:31 +0200

    🔑 CodeAuth | Database Connect added!

--- src/app.js
+++ src/app.js
@@ -2,14 +2,48 @@ const express = require('express')
 const app = express()
 const config = require('./config.json')
 const logger = require('./logger')
+const path = require('path')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
-app.get('/', function (req, res) {
-    res.send('test')
-})
+if (!config.mongoose_url) {
+    logger.failed('You have to set your MongoDB url.')
+} else if (!config.port) {
+    logger.failed('You have to set port')
+} else {
+    mongoose.connect(config.mongoose_url, {
+    }).then(() =>[
+        logger.success('Connected to database!')
+    ]).catch((err) =>{
+        logger.failed('Failed connect to the database!')
+    })
 
-app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '/pages/404.html')
-})
+    app.use(bodyParser.json()); 
 
-app.listen(config.port)
-logger.success(`Listening on port ${config.port}`)
+    app.use(bodyParser.urlencoded({ extended: true })); 
+
+    app.use(express.static(path.join(__dirname, 'pages')));
+
+
+
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/pages/index.html')
+    })
+
+    app.post('/usecode', function (req, res) {
+        const code = req.body.code
+        
+    })
+
+    app.get('/usecode', function (req, res) {
+        res.redirect('/')
+    })
+
+    app.get('/*', function (req, res) {
+        res.sendFile(__dirname + '/pages/404.html')
+    })
+
+    app.listen(config.port)
+    logger.success(`Listening on port ${config.port}`)
+
+}
--- src/logger.js
+++ src/logger.js
@@ -4,4 +4,9 @@ const success = function(msg) {
     console.log(colors.green('[SUCCESS] ') + msg)
 }
 
+const failed = function(msg) {
+    console.log(colors.red('[FAILED] ') + msg)
+}
+
 module.exports.success = success
+module.exports.failed = failed
